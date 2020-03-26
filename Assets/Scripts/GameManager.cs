c1a404cdc38d221ab315cbfd21fe079d472b38ff
Nick N <nicholas.ngoc.nguyen@gmail.com>
2020-03-26 00:01:32 -0700

    Created GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameState state;
+    public float gameTime;
+    public int score;
+
+    private void Update()
+    {
+        if(gameTime < 0 && state != GameState.End)
+        {
+            state = GameState.End;
+        }
+        else if(gameTime > 0)
+        {
+            gameTime -= Time.deltaTime;
+        }
+
+        //print(string.Format("GameState = {0}, Time = {1}", state, (int)gameTime));
+    }
+}
+
+public enum GameState
+{
+    Default,
+    End,
+}
