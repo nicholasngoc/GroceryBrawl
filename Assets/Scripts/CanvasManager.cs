999ba69c1d7549c5438e34ebc81e24a36f67f926
Nick N <nicholas.ngoc.nguyen@gmail.com>
2020-03-27 13:47:52 -0700

    Bags correspond to score and a canvas manager was made

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    public Text timerText;
+    public Text scoreText;
+
+    private void Update()
+    {
+        /**
+         * Note: as of right now these are only for testing.
+         * We can change how the UI is later
+         */
+
+        if(timerText != null)
+            timerText.text = "Time: " + (int)gameManager.gameTime;
+        if(scoreText.text != null)
+            scoreText.text = "Score: " + gameManager.playerScore.Score;
+    }
+}
---
+++ Assets/Scripts/ScoreModel.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This is a model class attached to both the player and
+/// enemies that handles the score system
+/// </summary>
+public class ScoreModel : MonoBehaviour
+{
+    private int _score;
+    public int Score
+    {
+        get
+        {
+            return _score;
+        }
+        set
+        {
+            _score = value;
+
+            //The following displays an amount of bags based on the score value
+            if(_bags != null)
+            {
+                for(int x = 0; x < _bags.Length; x++)
+                {
+                    if (x < _score)
+                        _bags[x].SetActive(true);
+                    else
+                        _bags[x].SetActive(false);
+                }
+            }
+        }
+    }
+    public Transform bagParent;
+    private GameObject[] _bags;
+
+    private void Awake()
+    {
+        _bags = new GameObject[bagParent.childCount];
+
+        for(int x = 0; x < _bags.Length; x++)
+        {
+            _bags[x] = bagParent.GetChild(x).gameObject;
+        }
+    }
+
+    private void Start()
+    {
+        //StartCoroutine(TestScore());
+    }
+
+    private IEnumerator TestScore()
+    {
+        for(int x = 0; x < _bags.Length; x++)
+        {
+            Score++;
+            print(Score);
+
+            yield return new WaitForSeconds(1);
+        }
+
+        for(int x = _bags.Length - 1; x >= 0; x--)
+        {
+            Score--;
+            print(Score);
+
+            yield return new WaitForSeconds(1);
+        }
+
+        yield return null;
+    }
+}
